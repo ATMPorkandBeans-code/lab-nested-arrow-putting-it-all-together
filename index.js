cae1411dc469933a89e8719b6954bd243c47838b
Adrian Moring <adriantmoringbass@gmail.com>
2025-08-09 16:18:26 -0400

    passes 3/5 tests

--- index.js
+++ index.js
@@ -4,3 +4,38 @@
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
 };
+
+function createLoginTracker(username, password){
+  const userInfo = {
+    username: username,
+    password: password
+    };
+
+    // console.log(userInfo)
+
+  let attemptCount = 0;
+
+  return (passwordAttempt) => {
+
+  attemptCount ++;
+
+  // console.log(attemptCount)
+
+  if(passwordAttempt === userInfo.password && attemptCount <= 3 ){
+    return "Login successful";
+   }
+  else if(passwordAttempt !== userInfo.password && attemptCount <= 3){
+    return `Attempt ${attemptCount}: Login failed`;
+  }
+  else if(attemptCount > 3){
+    return "Account locked due to too many failed login attempts";
+  }
+  }
+  };
+
+// const login = createLoginTracker("mockUser3", "heynow");
+
+// //console.log(login("wrong"));
+// console.log(login("heynow"));
+// // console.log(login("password123"));
+// // console.log(login("wrong"));
